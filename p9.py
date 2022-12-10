1ccd01aac414ea290aa53fb5f7537607bed8c8c8
TemporarilyTired <66777276+TemporarilyTired@users.noreply.github.com>
2022-12-10 20:01:10 +0100

    Completed problem 9

---
+++ p9.py
@@ -0,0 +1,13 @@
+def solve():
+    # 9: given (n=1000) such that a^2 + b^2 = c^2 and a + b + c = n has exactly one solution, find the product of a<b<c
+    # runtime: O(n^2)
+    # ans = 31875000
+    n = 1000
+
+    for a in range(1, n - 2):
+        a2 = a * a
+        for b in range(a + 1, n - a):
+            c2 = (n - a - b) * (n - a - b)
+            if a2 + b * b == c2:
+                return print(a * b * (n - a - b))
+    print("no solution for n=", n)
