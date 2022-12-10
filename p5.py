53cc93147077cb3d7f8e8cd4f7a346068b8fc220
TemporarilyTired <66777276+TemporarilyTired@users.noreply.github.com>
2022-12-10 17:59:55 +0100

    completed problem 5

---
+++ p5.py
@@ -0,0 +1,22 @@
+def solve():
+    # 5: the smallest number that is evenly (=without remainder) divisible by all numbers from 1 to n (= 20)
+    # runtime: O()
+    # ans = 232792560
+
+    n = 20
+
+    def lcm(a):
+        ans = 1
+        ps = []
+        for x in a:
+            for p in ps:
+                if not x % p:
+                    x //= p
+            if x:
+                ans *= x
+                ps.append(x)
+        return ans
+
+    n = lcm(range(1, n + 1))
+
+    print(n)
