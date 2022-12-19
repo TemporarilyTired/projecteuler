83186a36468fc89a5543081934175584b8e35a0b
TemporarilyTired <rens2.versnel@gmail.com>
2022-12-19 19:53:52 +0100

    Tweaked problem  18 input

--- 0/60/p67.py
+++ 0/60/p67.py
@@ -4,7 +4,6 @@ def solve():
     # 67: find the path (moving adjecent as in a symmetric triangle) from top to bottom with the largest sum
     # runtime: O(n^2) where n is the number of layers in the pyramid
     # ans = 7273
-    inp = ""
     with open("data/p67.txt", "r") as f:
         inp = f.read()
     print(m.max_triangle_path(inp))
