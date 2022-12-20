2e6c86bf0850111f9274e1e1d5f0c771d3a7ba5b
TemporarilyTired <rens2.versnel@gmail.com>
2022-12-20 21:03:48 +0100

    tweaked p25

--- 0/20/p25.py
+++ 0/20/p25.py
@@ -11,6 +11,35 @@ def solve():
     gr_num = sqrt5_num + sqrt5_denum
     igr_num = sqrt5_denum - sqrt5_num
 
+    def fib_approx_int(x):
+        return ((gr_num ** x - igr_num ** x) * sqrt5_denum >> x) // (sqrt5_num * sqrt5_denum ** x)
+
+    # find upper and lower limit
+    from math import log10
+    # use golden ratio = 1.61803398875 and add a small epsilon and substract a constant margin
+    margin = 3
+    ans = int(n / log10(1.61805)) - margin
+
+    fib_a = fib_approx_int(ans - 1)
+    fib_b = fib_approx_int(ans)
+    while fib_b < pow10n:
+        fib_a, fib_b, ans = fib_b, fib_a + fib_b, ans + 1
+    print(ans)
+
+
+def solve_v2():
+    # 25: Find i such that the ith fibonacci number is the first fibonacci number with n digits
+    # runtime: O(log n) if you assume the fibonacci approximation to be constant time,
+    # which it is not because the values can get arbitrarily large. So probably closer to O(n log n) but not sure..
+    # ans = 4782
+    n = 1000
+    pow10n = 10 ** (n - 1)
+
+    # we use rational approximations for sqrt(5) because of floating point size limits
+    sqrt5_num, sqrt5_denum = (161, 72) if n < 5000 else (5374978561, 2403763488)
+    gr_num = sqrt5_num + sqrt5_denum
+    igr_num = sqrt5_denum - sqrt5_num
+
     def fib_approx_exceeds_pown(x):
         return (gr_num ** x - igr_num ** x) * sqrt5_denum >> x > pow10n * (sqrt5_num * sqrt5_denum ** x)
 
