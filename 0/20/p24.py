0d6266f90d0d81423910666f292b7e0e1cdb1f9a
TemporarilyTired <rens2.versnel@gmail.com>
2022-12-20 15:30:23 +0100

    completed p24

--- 0/20/p24.py
+++ 0/20/p24.py
@@ -1,6 +1,25 @@
 def solve():
-    # 24: What is the nth permutation of the digits 1 through 9 when lexographically ordered
-    # runtime: O()
-    # ans =
+    # 24: What is the nth permutation of the digits 0 through 9 when lexographically ordered
+    # runtime: O(log n)
+    # ans = 2783915460
+
+    # we assume that n < (10!)
     n = 1_000_000
-    pass
+
+    d_scrambled = 1
+    fac = 1
+    while n > fac:
+        d_scrambled += 1
+        fac *= d_scrambled
+
+    d_sorted = 10 - d_scrambled
+    ans = "0123456789"[:d_sorted]
+    rest = list("0123456789"[d_sorted:])
+
+    # need to switch to 0-indexing for convenience
+    n -= 1
+    for div in range(d_scrambled, 0, -1):
+        fac //= div
+        d, n = divmod(n, fac)
+        ans += rest.pop(d)
+    print(ans)
